d50d02df824e133f65ac6c7a367b8f7553e2a4a8
Seth Delorme <delorme.s.m@gmail.com>
2021-02-28 13:31:18 -0600

    creates update action in controller and patch route

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -14,7 +14,21 @@ class Api::SongsController < ApplicationController
     render 'show.json.jb'
   end
   def show
-    @song = Song.find_by(id: params[:id])
+    if params[:id].to_i < Song.count
+      @song = Song.find_by(id: params[:id])
+    else
+      @song = Song.new
+    end
     render 'show.json.jb'
   end
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.title = params[:song] || @song.title
+    @song.artist = params[:artist] || @song.artist
+    @song.album = params[:album] || @song.album
+    @song.year = params[:year] || @song.year
+    @song.save
+    render 'show.json.jb'
+  end
+  
 end
