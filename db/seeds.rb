eab43316f8dabb915c2425b1029daeec5e196624
Seth Delorme <delorme.s.m@gmail.com>
2021-02-28 12:15:13 -0600

    creates seed file and seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+song = Song.new(title: "Self Control", artist: "Frank Ocean",album: "Blonde", year: 2016)
+song.save
+
+song = Song.new(title: "Try a Little Tenderness", artist: "Otis Redding",album: "Complete & Unbelievable", year: 1966)
+song.save
